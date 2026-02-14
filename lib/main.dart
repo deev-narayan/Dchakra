0de36828eb9a98650a1bc77f22492d66a00e300a
deev-narayan <svnarayan333@gmail.com>
2026-02-14 22:42:57 +0530

    supabase initialised

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:dchakra/pages/welcome_page.dart';
 import 'package:dchakra/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await dotenv.load(fileName: ".env");
+  
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const MyApp());
 }
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
