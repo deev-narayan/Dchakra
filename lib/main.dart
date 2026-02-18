eaa8db19b5019bb931160a8d4629e0bc0f6affb0
deev-narayan <svnarayan333@gmail.com>
2026-02-18 12:10:35 +0530

    made env and keys hided

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:dchakra/pages/welcome_page.dart';
 import 'package:dchakra/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
